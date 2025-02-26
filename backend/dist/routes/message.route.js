f991e89581badb15e085bf25997bdd6d62625f0b
Anadudev <anadudev@gmail.com>
2025-02-26 14:14:03 +0100

    refactor: remove unused backend files and update TypeScript configuration

--- backend/dist/routes/message.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { isProtected } from "../middleware/auth.middleware";
-import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller";
-const router = express.Router();
-router.get("/users", isProtected, getUsersForSidebar);
-router.get("/:id", isProtected, getMessages);
-router.post("/send/:id", isProtected, sendMessage);
-export default router;
